c8254e371d15bfe6ee76ade8728909e66e09cda0
yuha <dladbgkdla@gmail.com>
2025-11-24 10:39:28 +0900

    params: Promise → 그냥 params로 수정 { id } = params 로 변경

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,12 +1,14 @@
 import ProductDetailClient from "./ProductDetailClient";
 
-export default async function ProductPage({ params }: { params: Promise<{ id: string }> }) {
-  // ✅ Next.js 16부터 params는 Promise 형태임
-  const { id } = await params;
+export default async function ProductPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { id } = params;
 
-  // Spring Boot API 호출 (SSR)
   const res = await fetch(`http://localhost:8080/api/products/${id}`, {
-    cache: "no-store", // 항상 최신 데이터
+    cache: "no-store",
   });
 
   if (!res.ok) {
