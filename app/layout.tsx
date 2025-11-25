405d8883d415e2189e04fcbf0a4d9057543695a0
yuha <dladbgkdla@gmail.com>
2025-11-25 20:25:49 +0900

    RootLayout 클라이언트로 되어던거 서버 컴포넌트로 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,8 @@
-"use client";
-
 import "./globals.css";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 import { Providers } from "./providers";
-import { usePathname } from "next/navigation";
+import ClientRoot from "./ClientRoot";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-
-  // /admin으로 시작하면 관리자 페이지
-  const isAdmin = pathname.startsWith("/admin");
-
   return (
     <html lang="ko">
       <head>
@@ -19,17 +10,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
       <body className="flex flex-col min-h-screen">
         <Providers>
-          {/* 관리자 페이지가 아닐 때만 Header 출력 */}
-          {!isAdmin && <Header />}
-
-          <main className={`flex-1 bg-gray-100 overflow-x-hidden ${!isAdmin ? "py-16" : ""}`}>
-            <div className="mx-auto">
-              {children}
-            </div>
-          </main>
-
-          {/* 관리자 페이지가 아닐 때만 Footer 출력 */}
-          {!isAdmin && <Footer />}
+          <ClientRoot>{children}</ClientRoot>
         </Providers>
       </body>
     </html>
