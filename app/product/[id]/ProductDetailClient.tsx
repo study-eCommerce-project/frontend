64e8e6978190bf55b5fad1551336a4fec4561079
yuha <dladbgkdla@gmail.com>
2025-11-26 16:12:01 +0900

    병합

--- app/product/[id]/ProductDetailClient.tsx
+++ app/product/[id]/ProductDetailClient.tsx
@@ -1,20 +1,16 @@
 "use client";
 
-<<<<<<< HEAD
 import { useEffect, useState } from "react";
-import ProductDetailTop from "@/components/product/ProductDetailTop";
-import ProductDetailBottom from "@/components/product/ProductDetailBottom";
-=======
 import ProductDetailTop from "@/app/product/components/ProductDetailTop";
 import ProductDetailBottom from "@/app/product/components/ProductDetailBottom";
->>>>>>> daehyun
 
 
 export default function ProductDetailClient({ productId }: { productId: number }) {
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
   const [product, setProduct] = useState(null);
 
   useEffect(() => {
-    fetch(`http://localhost:8080/api/products/${productId}/detail`, {
+    fetch(`${API_URL}/api/products/${productId}/detail`, {
       credentials: "include",
     })
       .then(res => res.json())
